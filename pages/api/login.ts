c2ce26e39672ea2d8a8f60d99753e955d6a592b9
Edilson Junior <102622957+edrsjunior@users.noreply.github.com>
2023-11-27 15:16:06 +0000

    jwt implents, need create a JWT STRING IN LOCAL ENV

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,13 +1,23 @@
 import type { NextApiHandler,NextApiRequest,NextApiResponse } from "next";
 import { conectarMongoDB } from "../../middlewares/conectaMongoDB";
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+import type { LoginResposta } from "../../types/LoginResposta";
 import md5 from 'md5';
 import { UsuarioModel } from "../../models/userModel";
+import jwt from 'jsonwebtoken';
+
 
 const endpointLogin =  async (
     req: NextApiRequest,
-    res: NextApiResponse<RespostaPadraoMsg>
+    res: NextApiResponse<RespostaPadraoMsg | LoginResposta>
 ) => {
+
+    const {MINHA_CHAVE_JWT} = process.env;
+
+    if(!MINHA_CHAVE_JWT){
+        res.status(500).json({error:'ENV JWT not sended'})
+    }
+
     if (req.method === 'POST') {
         const {login, senha} = req.body; //DESESTRURACAO DE OBEJTOS ONDE CRIO DUAS VARIAVEIS CONST QUE RECEBEM OS VALORES DE LOGIN E SENHA NO req.body
         
@@ -15,8 +25,12 @@ const endpointLogin =  async (
 
 
         if (usersFind && usersFind.length > 0) {
+
                 const userFounded = usersFind[0];
-                return res.status(200).json({msg:`Hello, ${userFounded.nome} login complete with success`}); 
+
+                const token = jwt.sign({_id: userFounded.id},MINHA_CHAVE_JWT);
+
+                return res.status(200).json({nome: userFounded.nome, email: userFounded.email, token}); 
         }
         return res.status(400).json({error:"User or passoword not found"});
             
