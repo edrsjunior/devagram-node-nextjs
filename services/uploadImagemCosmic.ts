3ade35de95a537e814ba89ef18e870200c399102
Edilson Junior <edilsonrsjunior@gmail.com>
2023-12-04 22:23:13 -0400

    create middleware to upload imgs to cosmic

---
+++ services/uploadImagemCosmic.ts
@@ -0,0 +1,53 @@
+import multer from "multer";
+import comsmicjs from comsmicjs;
+import { createBucketClient } from '@cosmicjs/sdk';
+
+
+const {CHAVE_LEITURA_SLUG,
+    CHAVE_GRAVACAO_SLUG,
+    CHAVE_SLUG,} = process.env;
+
+    
+
+// Authenticate
+const bucketDevagram = createBucketClient({
+  bucketSlug: CHAVE_SLUG as string,
+  readKey: CHAVE_LEITURA_SLUG as string,
+  writeKey: CHAVE_GRAVACAO_SLUG as string,
+});
+
+
+const storage = multer.memoryStorage();
+const upload = multer({storage : storage});
+
+const uploadImagemCosmic = async(req: any) =>{
+
+    // Fetch content
+    await bucketDevagram.objects
+    .find({
+    type: 'posts',
+    })
+    .limit(1);
+
+    if (req?.file?.originalmame) {
+        const media_object = {
+            originalName: req.file.originalmame,
+            buffer : req.file.buffer
+        }
+
+        if (req.url && req.url.includes('publicacao')) {
+            return await bucketDevagram.media.insertOne({
+                media: media_object,
+                folder: "publicacoes",
+            });
+        }
+        else{
+            return await bucketDevagram.media.insertOne({
+                media: media_object,
+                folder: "avatares",
+            });
+        }
+    }
+}
+
+export {upload, uploadImagemCosmic};
